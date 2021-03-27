7fa9994e97fd8e2de46b01d9b11b4598205ed9cb
nMusacchio <42496193+nMusacchio@users.noreply.github.com>
2021-03-27 14:48:38 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,8 +7,19 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Juju, esto no es Coca papi
+          Hola, este es el proyecto de Ignacio.
         </p>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
